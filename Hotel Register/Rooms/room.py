949ca46026257ca07922481142d122b1cc81d939
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2021-09-09 22:49:14 +0300

    Add "guests_info" parameter to the Room class

--- Hotel Register/Rooms/room.py
+++ Hotel Register/Rooms/room.py
@@ -1,9 +1,10 @@
 class Room:
     """
-    This is the base class for all other rooms
+    This is the base class for all other rooms.
     """
     def __init__(self, number, max_capacity, price_per_night):
         self.number = number
         self.max_capacity = max_capacity
         self.price_per_night = price_per_night
         self.occupied = False
+        self.guests_info = []
