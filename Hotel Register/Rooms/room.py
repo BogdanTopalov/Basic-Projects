13f566db29ba9bac8f3890ecb970b33c50ff2305
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2021-09-12 13:02:55 +0300

    Add new methond to room.py

--- Hotel Register/Rooms/room.py
+++ Hotel Register/Rooms/room.py
@@ -8,3 +8,13 @@ class Room:
         self.occupied = False
         self.cleaned = True
         self.guests_info = []
+
+    def get_guest_info(self):
+        info = ["Guests in the room: "]
+
+        for guest in self.guests_info:
+            name = guest.full_name
+            age = guest.age
+            info.append(f"> {name} - {age} years old.")
+
+        return "\n".join(info)
