464fbec64198dc2e0b5114ca04ce16dd42e5b0be
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2021-10-13 20:09:37 +0300

    Add Person import and write check_in method

--- Gym/gym.py
+++ Gym/gym.py
@@ -1,14 +1,31 @@
+from Gym.person import Person
+
+
 class Gym:
-    def __init__(self, max_capacity):
+    def __init__(self, name, max_capacity):
+        self.name = name
         self.max_capacity = max_capacity
-        self.currently_in = {}
+        self.currently_in = []
 
-    def check_in(self, person):
-        if person.active_membership:
-            pass
+    def check_in(self, person: Person):
 
-        else:
-            self.currently_in[person.name] = person
+        # Check if person has active membership.
+        if not person.membership:
+            return f"{person.name}'s membership is not active. " \
+                   f"Please active it before checking in."
+
+        # Check if person has clean shoes.
+        if not person.has_clean_shoes:
+            return f"{person.name} doesn't have clean shoes. " \
+                   f"In order to be checked in, {person.name} needs to have clean shoes."
+
+        # Check if gym is full.
+        if len(self.currently_in) >= self.max_capacity:
+            return f"{self.name} Gym is full at the moment. " \
+                   f"Try checking in later."
+
+        # Add person instance in the list.
+        self.currently_in.append(person)
 
     def info(self):
         return len(self.currently_in)
